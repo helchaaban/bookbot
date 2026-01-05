ccf320b2e4dd8e9e781e8a791a984d6f8843fd57
helchaaban <hassanshaaban99@gmail.com>
2026-01-05 17:04:25 -0500

    added sys import

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from stats import get_words, get_letters, sort_dict
-
+from stats import get_words, get_letters, chars_dict_to_sorted_list
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
-    #print(text)
+    print("=============BOOKBOT=============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------------WORD COUNT---------")
     get_words(text)
-    sort_dict(get_letters(text))
+    print("------------------Character Count-----------")
+    sorted_dict_list = chars_dict_to_sorted_list(get_letters(text))
+
+    for item in sorted_dict_list:
+        ch = item["char"]
+        num = item["num"]
+        print(f"{ch}: {num}")
     
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -4,7 +4,9 @@
 
 def get_words(booktext):
     word_count = len(booktext.split())
-    return print(f"Found {word_count} total words")
+    print(f"Found {word_count} total words")
+    return word_count  # no print here
+
 
 def get_letters(booktext):
     letter_count = {}
@@ -14,9 +16,21 @@ def get_letters(booktext):
             letter_count[letter] = 1
         else:
             letter_count[letter] = letter_count[letter] + 1
+    return letter_count  # return the dict, don't print
 
     
-    return print(letter_count)
+    return letter_count
 
-def sort_dict(dict):
-    dict.sort()
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        if ch.isalpha():
+            sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+        else:
+            continue
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
